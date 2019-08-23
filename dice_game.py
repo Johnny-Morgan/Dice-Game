a8df74455487f440777900a11e444fb5f2c644f2
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-08-23 01:43:56 +0100

    Added code for playGame & stop functions

--- dice_game.py
+++ dice_game.py
@@ -1,9 +1,13 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFont, QPixmap
+from PyQt5.QtCore import QTimer
+from random import randint
 
 textFont = QFont("Times", 14)
-buttonsFont = QFont("Arial", 12)
+buttonFont = QFont("Arial", 12)
+computerScore = 0
+playerScore = 0
 
 
 class Window(QWidget):
@@ -15,10 +19,10 @@ class Window(QWidget):
 
     def UI(self):
         # Scores
-        self.scoreComputerText = QLabel("Computer Score: ", self)
+        self.scoreComputerText = QLabel("Computer Score:   ", self)
         self.scoreComputerText.move(30, 20)
         self.scoreComputerText.setFont(textFont)
-        self.scorePlayerText = QLabel("Player Score: ", self)
+        self.scorePlayerText = QLabel("Player Score:   ", self)
         self.scorePlayerText.move(340, 20)
         self.scorePlayerText.setFont(textFont)
 
@@ -34,7 +38,80 @@ class Window(QWidget):
         self.imageVersus = QLabel(self)
         self.imageVersus.setPixmap(QPixmap("images/versus.png"))
         self.imageVersus.move(230, 135)
+
+        # Buttons
+        btnStart = QPushButton("Start", self)
+        btnStart.setFont(buttonFont)
+        btnStart.move(150, 250)
+        btnStart.clicked.connect(self.start)
+        btnStop = QPushButton("Stop", self)
+        btnStop.setFont(buttonFont)
+        btnStop.move(260, 250)
+        btnStop.clicked.connect(self.stop)
+
+        # Timer
+        self.timer = QTimer(self)
+        self.timer.setInterval(100)
+        self.timer.timeout.connect(self.playGame)
         self.show()
+
+    def start(self):
+        self.timer.start()
+
+    def stop(self):
+        global computerScore
+        global playerScore
+        self.timer.stop()
+
+        if self.rndComputer == self.rndPlayer:
+            QMessageBox.information(self, "Information", "Draw Game")
+        elif self.rndComputer > self.rndPlayer:
+            QMessageBox.information(self, "Information", "Computer Wins")
+            computerScore += 1
+            self.scoreComputerText.setText("Computer Score: {}".format(computerScore))
+            self.scorePlayerText.setText("Player Score: {}".format(playerScore))
+        else:
+            QMessageBox.information(self, "Information", "You win")
+            playerScore += 1
+            self.scoreComputerText.setText("Computer Score: {}".format(computerScore))
+            self.scorePlayerText.setText("Player Score: {}".format(playerScore))
+
+        if computerScore == 5:
+            QMessageBox.information(self, "Information", "Game Over\nComputer wins")
+            sys.exit()
+        elif playerScore == 5:
+            QMessageBox.information(self, "Information", "Game Over\nYou win")
+            sys.exit()
+
+
+    def playGame(self):
+        self.rndComputer = randint(1, 6)
+        if self.rndComputer == 1:
+            self.imageComputer.setPixmap(QPixmap("images/dice_1.png"))
+        elif self.rndComputer == 2:
+            self.imageComputer.setPixmap(QPixmap("images/dice_2.png"))
+        elif self.rndComputer == 3:
+            self.imageComputer.setPixmap(QPixmap("images/dice_3.png"))
+        elif self.rndComputer == 4:
+            self.imageComputer.setPixmap(QPixmap("images/dice_4.png"))
+        elif self.rndComputer == 5:
+            self.imageComputer.setPixmap(QPixmap("images/dice_5.png"))
+        else:
+            self.imageComputer.setPixmap(QPixmap("images/dice_6.png"))
+
+        self.rndPlayer = randint(1, 6)
+        if self.rndPlayer == 1:
+            self.imagePlayer.setPixmap(QPixmap("images/dice_1.png"))
+        elif self.rndPlayer == 2:
+            self.imagePlayer.setPixmap(QPixmap("images/dice_2.png"))
+        elif self.rndPlayer == 3:
+            self.imagePlayer.setPixmap(QPixmap("images/dice_3.png"))
+        elif self.rndPlayer == 4:
+            self.imagePlayer.setPixmap(QPixmap("images/dice_4.png"))
+        elif self.rndPlayer == 5:
+            self.imagePlayer.setPixmap(QPixmap("images/dice_5.png"))
+        else:
+            self.imagePlayer.setPixmap(QPixmap("images/dice_6.png"))
 
 
 def main():
